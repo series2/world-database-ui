fc129f652221e9df38dceb59112fa88f02fd0de4
series2 <series2.others@gmail.com>
2019-07-09 21:21:32 +0900

    test

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>|
+      <router-link to="/axios">Axios</router-link>|
+      <router-link to="/Tokyotest">Tokyotest</router-link>|
+      <router-link to="/city/Tokyo">Tokyo</router-link>|
+      <router-link to="/login">Login</router-link>|
+      <router-link to="/search">Search</router-link>
+      <!--ここのリンクを基にrouter.jsでリンク先を作って指定する-->
+    </div>
+    <router-view />
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -23,6 +21,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
